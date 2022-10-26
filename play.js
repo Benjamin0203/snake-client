8d6e42ad11cbf882f1e73ffd51e4327f3c83212f
Benjamin Meng <benjaminmeng1994@gmail.com>
2022-10-26 14:48:04 -0700

    input.js added

--- play.js
+++ play.js
@@ -1,26 +1,11 @@
 const net = require("net");
-const {connect} = require("./client");
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-const setupInput = () => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on('data', key => {
-    handleUserInput(key);
-  })
-  return stdin;
 
-};
 
-const handleUserInput = (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    };
-  
-};
-
-setupInput();
 
 console.log("Connecting ...");
 connect();
+
+setupInput();
